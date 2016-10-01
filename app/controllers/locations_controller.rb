d8dbe73c888017ef375b7fc7955775c66be798cf
chunchiar <chunchiar_7@hotmail.com>
2016-10-01 22:07:29 +0800

    done some stuff

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -19,10 +19,11 @@ class LocationsController < ApplicationController
       marker.lat chef.latitude
       marker.lng chef.longitude
       marker.infowindow render_to_string(partial: "/locations/infowindow", :locals => {:chefs => chef})
-      #marker.picture({
-        #"url" => "user.image.url(:thumb)",
-        #"width" => 32,
-        #"height" => 32})
+      #"user.image.url(:thumb)",
+      marker.picture({
+        "url" => "https://41.media.tumblr.com/028c5097d2e537595c86a81934b83cbf/tumblr_o4u8o54tpv1qjknfdo1_75sq.png",
+        "width" => 32,
+        "height" => 32})
     end
   end
 
