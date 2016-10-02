e656900b01fba939d34f2b775afe204344a8379d
chunchiar <chunchiar_7@hotmail.com>
2016-10-02 13:14:48 +0800

    commit

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -21,7 +21,7 @@ class LocationsController < ApplicationController
       marker.infowindow render_to_string(partial: "/locations/infowindow", :locals => {:chefs => chef})
       #"user.image.url(:thumb)",
       marker.picture({
-        "url" => "https://41.media.tumblr.com/028c5097d2e537595c86a81934b83cbf/tumblr_o4u8o54tpv1qjknfdo1_75sq.png",
+        "url" => "http://i1174.photobucket.com/albums/r618/mashstore/spoon_marker%201_zpspa5ktwyl.png",
         "width" => 32,
         "height" => 32})
     end
