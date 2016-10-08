beb7ea6e3117db9c423fa67dfb60a4ad37f12ac9
Edwin Yiing <edwinyiing@gmail.com>
2016-10-08 09:59:17 +0800

    updated landing page items

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -3,9 +3,9 @@ class LandingController < ApplicationController
   def index
 
     # @latest = Product.order(created_at: :desc)
-    @latest = Product.where("expire > ?", DateTime.now).order(created_at: :desc).first(3)
-    @just_sold = Order.order(created_at: :desc).first(3)
-    @expiring = Product.where("expire > ?", DateTime.now).order(expire: :asc).first(3)
+    @latest = Product.where("expire > ?", DateTime.now).order(created_at: :desc).first(9)
+    @just_sold = Order.order(created_at: :desc).first(9)
+    @expiring = Product.where("expire > ?", DateTime.now).order(expire: :asc).first(9)
     # map function
 
     runmap
